e97d3253de1f023a5b3fcb4278ab880e95a2ba46
Elías Puma <elpumitaelias@gmail.com>
2017-09-01 18:18:12 -0500

    refactored

--- word_counter/src/main.rs
+++ word_counter/src/main.rs
@@ -1,7 +1,6 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-// use std::collections::HashMap;
 use std::collections::BTreeMap;
 
 fn sanitize_word(input: &str) -> String {
@@ -10,19 +9,15 @@ fn sanitize_word(input: &str) -> String {
 }
 
 fn main() {
-    let mut f = File::open("data/BOM.txt").expect("Error opening file");
-    let mut reader = BufReader::new(f);
+    let file = File::open("data/BOM.txt").expect("Error opening file");
+    let mut reader = BufReader::new(file);
 
-    // let mut frequency: HashMap<String, u32> = HashMap::new();
     let mut frequency: BTreeMap<String, u32> = BTreeMap::new();
 
     for line in reader.lines() {
         if let Ok(line) = line {
-            // println!("{}", line);
-
             for word in line.split_whitespace() {
                 *frequency.entry(sanitize_word(word)).or_insert(0) += 1;
-                // print!("{} ", word)
             }
         } else {
             println!("Error reading line");
